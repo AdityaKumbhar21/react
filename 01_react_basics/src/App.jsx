6720bb02aa183fecb1b70f478c91a4d3782030b3
AdityaKumbhar21 <adityakumbhar915@gmail.com>
2023-10-20 09:05:38 +0530

    imported Hello

--- 01_react_basics/src/App.jsx
+++ 01_react_basics/src/App.jsx
@@ -1,8 +1,16 @@
+import Hello from "./Hello"
+
 function App() {
 
 
   return (
-   <h1>React Tutorials | Aditya Kumbhar | Studied how to install react using react as well as vite | Also studied the basic file flow of react</h1>
+   // in return we can only return one element
+   // <></> This is known as html Fragment 
+  <>  
+
+    <h1>React Tutorials | Aditya Kumbhar | Studied how to install react using react as well as vite | Also studied the basic file flow of react</h1>
+    <Hello/>
+  </>
   )
 }
 
--- 01_react_basics/src/main.jsx
+++ 01_react_basics/src/main.jsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(  // render is used to render the app in the root div
   <React.StrictMode>
-    <App />
+   
+    <App />    {/* This is basically a customized HTML tag created using react  */}
   </React.StrictMode>,
 )
