ff8c2a54a908b5dcfbb05ecffad9900de048d2e5
AdityaKumbhar21 <adityakumbhar915@gmail.com>
2023-10-20 09:05:19 +0530

    added an hello file to understand basic imp exp

---
+++ 01_react_basics/src/Hello.jsx
@@ -0,0 +1,10 @@
+// The naming convention in vite is you should give the file name with first letter uppercase
+// when youa are exporting a function the function name's first letter should be capital
+
+function Hello() {
+   return(
+    <h3>Hello World</h3>
+   );
+}
+
+export default Hello;
