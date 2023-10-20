3e83e63e7bd119d369c543da08889455d0f6f737
AdityaKumbhar21 <adityakumbhar915@gmail.com>
2023-10-20 08:30:22 +0530

    basic react

---
+++ 01_react_basics/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+
+
+  return (
+   <h1>React Tutorials | Aditya Kumbhar | Studied how to install react using react as well as vite | Also studied the basic file flow of react</h1>
+  )
+}
+
+export default App
