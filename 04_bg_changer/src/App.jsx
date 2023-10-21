b2bc86e41c645400cac2eeb1fe4f254ac7efcf5b
AdityaKumbhar21 <adityakumbhar915@gmail.com>
2023-10-21 17:26:17 +0530

    added the final bg changer file

--- 04_bg_changer/src/App.jsx
+++ 04_bg_changer/src/App.jsx
@@ -2,12 +2,31 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [color,setColor] = useState("blue")
+  let [color,setColor] = useState("blue")
 
   return (
-   <>
-    
-   </>
+
+      <div className="w-full h-screen duration-200" style={{backgroundColor: color}}>
+        <div className=' fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+          <div className='fixed flex flex-wrap justify-center  gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+            <button onClick={()=>setColor("red")} className='outline-none rounded-lg px-2 text-white'style={{backgroundColor: "red"}}
+            >Red</button>
+
+            <button className='outline-none rounded-lg px-2 text-white'style={{backgroundColor: "blue"}}
+            onClick={()=>setColor("blue")}>Blue</button>
+
+            <button className='outline-none rounded-lg px-2 text-white'style={{backgroundColor: "green"}}
+            onClick={()=>setColor("green")}>Green</button>
+
+            <button className='outline-none rounded-lg px-2 text-white'style={{backgroundColor: "purple"}}
+            onClick={()=>setColor("purple")}>Purple</button>
+
+            <button className='outline-none rounded-lg px-2 text-white'style={{backgroundColor: "yellow"}}
+            onClick={()=>setColor("yellow")}>Yellow</button>
+
+          </div>
+        </div>
+      </div>
   )
 }
 
