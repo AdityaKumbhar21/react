816f6a7d4755083e63ca359fa295f56d67b920d5
Aditya Kumbhar <adityakumbhar915@gmail.com>
2023-11-14 12:51:14 +0530

    added loader to optimize loading time

--- 07ReactRouter/src/components/Github/Github.jsx
+++ 07ReactRouter/src/components/Github/Github.jsx
@@ -1,19 +1,21 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
-
+import { useLoaderData } from "react-router-dom";
 
 function Github(){
 
-    const [data, setData] = useState([])
+    // const [data, setData] = useState([])
 
-    useEffect(()=>{
-        fetch("https://api.github.com/users/hiteshchoudhary")
-        .then(response => response.json())
-        .then(jsonResponse => {
-            setData(jsonResponse)
-        })
-    })
+    // useEffect(()=>{
+    //     fetch("https://api.github.com/users/hiteshchoudhary")
+    //     .then(response => response.json())
+    //     .then(jsonResponse => {
+    //         setData(jsonResponse)
+    //     })
+    // })
     
+    const data = useLoaderData()
+
     return(
         <>
         <div className=" text-white text-3xl bg-gray-700 p-4">
@@ -26,3 +28,8 @@ function Github(){
 }
 
 export default Github
+export const githubInfoPage = async ()=>{
+    const response = await fetch("https://api.github.com/users/hiteshchoudhary")
+
+    return response.json()
+}
--- 07ReactRouter/src/main.jsx
+++ 07ReactRouter/src/main.jsx
@@ -7,7 +7,7 @@ import Home from "./components/Home/Home.jsx"
 import About from "./components/About/About.jsx"
 import Contact from "./components/Contact/Contact.jsx"
 import User from "./components/User/User.jsx"
-import Github from './components/Github/Github.jsx'
+import Github, {githubInfoPage} from './components/Github/Github.jsx'
 
 
 // const router = createBrowserRouter([{
@@ -35,7 +35,10 @@ const router = createBrowserRouter(
       <Route path='about' element={<About />} />
       <Route path='contactus' element={<Contact />} />
       <Route path='user/:userId' element={<User />} />
-      <Route path='github' element={<Github />} />
+      <Route 
+      loader={githubInfoPage}
+      path='github' 
+      element={<Github />} />
   </Route>
   )
 )
